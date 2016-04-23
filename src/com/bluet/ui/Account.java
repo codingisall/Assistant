c192d084b65e43058a551fc8672739227f325dad
leoge <gguangle@gmail.com>
2016-04-23 17:30:58 +0800

    Add missing files

---
+++ src/com/bluet/ui/Account.java
@@ -0,0 +1,20 @@
+package com.bluet.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bluet.massistant.BaseFragment;
+import com.bluet.massistant.R;
+
+
+public class Account extends BaseFragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.accounts, null, false);
+        getActivity().getWindow().setTitle("血液回收机");
+        return view;
+    }
+
+}
